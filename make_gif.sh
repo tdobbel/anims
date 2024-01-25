e8c9344be5efde8021bdd15037d5481fc12558be
Dobby <thomas.dobbelaere@uclouvain.be>
2024-01-25 17:10:38 +0100

    make_gif now executable

--- make_gif.sh
+++ make_gif.sh
@@ -1,10 +1,20 @@
-frame_rate=6
-anim_name=anim_qaruh_streamlines
+#!/bin/bash
 
+lossy=80
 
-ffmpeg -y -r $frame_rate -f image2 -vb 5000k -i anim/frame_%04d.png -vf "scale=trunc(iw/4)*2:trunc(ih/4)*2" -pix_fmt yuv420p $anim_name.mp4
-ffmpeg -y -i $anim_name.mp4 -filter_complex "[0:v] palettegen" palette.png
-ffmpeg -y -i $anim_name.mp4 -i palette.png -filter_complex "[0:v][1:v] paletteuse" $anim_name.gif
-optimized=$(printf "%s_optimized.gif" $anim_name)
-gifsicle -O3 --lossy=80 --colors 256 $anim_name.gif -o $optimized
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -r|--frame-rate) frame_rate=$2; shift;;
+        -f|--frame-format) frame_format="$2"; shift;;
+        -o|--output-name) output_name="$2"; shift;;
+        -l|--lossy) lossy="$2"; shift;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
 
+ffmpeg -y -r $frame_rate -f image2 -vb 5000k -i anim/frame_%04d.png -vf "scale=trunc(iw/4)*2:trunc(ih/4)*2" -pix_fmt yuv420p $output_name.mp4
+ffmpeg -y -i $output_name.mp4 -filter_complex "[0:v] palettegen" palette.png
+ffmpeg -y -i $output_name.mp4 -i palette.png -filter_complex "[0:v][1:v] paletteuse" $output_name.gif
+optimized=$(printf "%s_optimized.gif" $output_name)
+gifsicle -O3 --lossy=$lossy --colors 256 $output_name.gif -o $optimized
